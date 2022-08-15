9bab309475349a366c7dbdf7be5b0ca10229e13e
Jean 'henyxia' Wasilewski <henyxia@revs0.com>
2022-08-15 17:13:40 +0200

    feat: new index

--- gis5ai/server/views.py
+++ gis5ai/server/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Team
+
+def index(request):
+    num_teams = Team.objects.all().count()
+
+    context = {
+        'num_teams': num_teams,
+    }
+
+    return render(request, 'index.html', context=context)
