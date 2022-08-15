08cc00e6a045b1a3cc2785b7f01d7b3741fc578b
Jean 'henyxia' Wasilewski <henyxia@revs0.com>
2022-08-15 13:29:18 +0200

    feat(model): add members of team

--- gis5ai/server/models.py
+++ gis5ai/server/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=100, help_text='Enter your team name')
+    member1 = models.CharField(max_length=8, help_text='login8 of the first member', default='login8')
+    member2 = models.CharField(max_length=8, help_text='login8 of the second member', blank=True)
+    member3 = models.CharField(max_length=8, help_text='login8 of the third member', blank=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('team-detail-view', args=[str(self.id)])
